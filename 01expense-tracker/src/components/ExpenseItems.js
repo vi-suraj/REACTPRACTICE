35228c3feeb31123658e766dbd22efee2ce5fe5f
SurajVishw <surajvishwakarma10234@gmail.com>
2025-01-06 23:23:23 +0530

    props and components details

---
+++ 01expense-tracker/src/components/ExpenseItems.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./ExpenseItems.css";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItems(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+// passing props using second approch object destruturing
+
+// function ExpenseItems({ date, title, amount }) {
+//   return (
+//     <div className="expense-item">
+//       <div>{date.toISOString()}</div>
+//       <div className="expense-item__description">
+//         <h2>{title}</h2>
+//         <div className="expense-item__price">${amount}</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+export default ExpenseItems;
