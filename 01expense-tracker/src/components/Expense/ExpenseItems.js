7fdfb91d897752e1693f608cbb36972c656d6fb4
SurajVishw <surajvishwakarma10234@gmail.com>
2025-01-08 23:51:08 +0530

    use state in detail

--- 01expense-tracker/src/components/Expense/ExpenseItems.js
+++ 01expense-tracker/src/components/Expense/ExpenseItems.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItems.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
+// how stats works under the hood
+
+// when you intialize the state it store the value of props.title in title for the first time
+
+// when the click event occur then state update occur where we tell react to re render the component where the state changed
+
+// when react re render that component it remembers that the stats was rendered before and take the setTitle value and change the value of the title
+
+// in the state we use const because we do not assign value using assignment operator we pass value to setTitle where react handle the value
+
 function ExpenseItems(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const titleHandler = () => {
+    setTitle("Updated");
+  };
+
   return (
     <Card className="expense-item">
       {/* <div>{props.date.toISOString()}</div> */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={titleHandler}>Update Title</button>
       </div>
     </Card>
   );
