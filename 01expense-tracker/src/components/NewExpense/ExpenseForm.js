7595e3da50a8c42f22f73e4c72912bbe40804a8d
SurajVishw <surajvishwakarma10234@gmail.com>
2025-01-13 23:51:09 +0530

    chart bar

--- 01expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ 01expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -42,26 +42,35 @@ const ExpenseForm = (props) => {
     props.onSaveExpenseData(expenseData);
   };
 
+  const [formShow, setFormShow] = useState(false);
+
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+    <div>
+      <form onSubmit={submitHandler}>
+        {formShow && (
+          <div className="new-expense__controls ">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input type="number" value={enteredAmount} onChange={amountChangeHandler} />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input type="date" min="2023-01-01" max="2026-01-01" value={enteredDate} onChange={dateChangeHandler} />
+            </div>
+          </div>
+        )}
+        <div className={formShow === true ? "new-expense__actions" : "new-expense__actions center"}>
+          {formShow && <button type="submit">Add Expense</button>}
+          <button type="text" onClick={() => setFormShow(!formShow)}>
+            {formShow === true ? "Cancel" : "Add New Expense"}
+          </button>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input type="number" value={enteredAmount} onChange={amountChangeHandler} />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input type="date" min="2023-01-01" max="2026-01-01" value={enteredDate} onChange={dateChangeHandler} />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
